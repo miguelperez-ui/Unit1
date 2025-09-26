8b1716f124150fd81c996d241193c6033ac418bf
miguel.perez <miguel.perez@106-20.info2>
2025-09-26 13:45:06 +0200

    Ejercicos modelo exámen

---
+++ src/unit1/ExamenModelo1.java
@@ -0,0 +1,54 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class ExamenModelo1 {
+
+	public static void main(String[] args) {
+		
+		//nuevo scanner
+		Scanner reader = new Scanner(System.in);
+		
+		//primer número
+		int primero;
+		
+		//segundo número
+		int segundo;
+		
+		//tercer número
+		int tercero;
+		
+		//media de los tres números
+		double media;
+		
+		//mensaje por consola
+		System.out.println("dime el primer número");
+		
+		//leer mensaje
+		primero = reader.nextInt();
+		
+		//mensaje por consola
+		System.out.println("dime el segundo número");
+		
+		//leer mensaje
+		segundo = reader.nextInt();
+		
+		//mensaje por consola
+		System.out.println("dime el tercer número");
+		
+		//leer mensaje
+		tercero = reader.nextInt();
+		
+		//ecuación para hacer la media
+		media = (primero + segundo + tercero)/ 3.0;
+		
+		//mensaje final
+		System.out.println("La media es: " + media);
+		
+		
+		//cerra scanner
+		reader.close();
+
+	}
+
+}
---
+++ src/unit1/ExamenModelo2.java
@@ -0,0 +1,10 @@
+package unit1;
+
+public class ExamenModelo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
